de4afd9eeaa107a5add66626843305b7e466791c
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2023-11-11 16:20:25 -0500

    Cerrar sesión y recuperar contraseña

--- src/app/modulos/seguridad/cerrar-sesion/cerrar-sesion.component.ts
+++ src/app/modulos/seguridad/cerrar-sesion/cerrar-sesion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cerrar-sesion',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./cerrar-sesion.component.css']
 })
 export class CerrarSesionComponent {
+  constructor(
+    private servicioSeguridad: SeguridadService,
+    private router: Router
+  ) { }
 
+  ngOnInit() {
+    this.cerrarSesión();
+  }
+
+  cerrarSesión() {
+    this.servicioSeguridad.RemoverDatosUsuarioValidado();
+    this.router.navigate(['']);
+  }
 }
